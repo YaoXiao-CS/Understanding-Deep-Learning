25c673a1f2eb9d50e390309e095d70bcc80e68a9
YaoXiao-CS <2736658492@qq.com>
2024-10-30 14:47:05 +0800

    Rectified some spelling errors.

--- Appendix files/Update_requirements_txt.py
+++ Appendix files/Update_requirements_txt.py
@@ -7,22 +7,24 @@ prompt-toolkit @ file:///C:/b/abs_6coz5_9f2s/croot/prompt-toolkit_1672387908312/
 其他人在使用pip install -r requirements.txt的时候报错,这里对相应的行进行删除.
 """
 
-# 定义文件路径.
-file_path = '../requirements.txt'
+def filter_requirements(file_path):
+    # 读取文件内容, 使用utf-8编码.
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+    except UnicodeDecodeError:
+        print("文件编码可能不是UTF-8，请重新检查文件编码。")
+        return
 
-# 读取文件内容, 使用utf-8编码.
-try:
-    with open(file_path, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-except UnicodeDecodeError:
-    print("文件编码可能不是UTF-8，请重新检查文件编码。")
-    exit()
+    # 过滤掉包含@符号的行.
+    filtered_lines = [line for line in lines if '@' not in line]
 
-# 过滤掉包含@符号的行.
-filtered_lines = [line for line in lines if '@' not in line]
+    # 将修改后的内容写回文件, 使用utf-8编码.
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.writelines(filtered_lines)
 
-# 将修改后的内容写回文件, 使用utf-8编码.
-with open(file_path, 'w', encoding='utf-8') as file:
-    file.writelines(filtered_lines)
+    print("已删除所有包含@符号的行, 并更新了requirements.txt文件。")
 
-print("已删除所有包含@符号的行, 并更新了requirements.txt文件。")
+# 调用函数
+filter_requirements('../requirements.txt')
+
